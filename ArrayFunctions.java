b1752a6a9cd33e02966e618b689132f5345cd63a
parth bhutada <parthbhutada13@gmail.com>
2025-02-03 20:38:48 +0530

    Create ArrayFunctions.java using array lists

---
+++ ArrayFunctions.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+class ArrayFunctions {
+    public void display(ArrayList<Integer> array) {
+        System.out.println("Array is: " + array); // Using ArrayLists
+    }
