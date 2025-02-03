7d40c880aea7560242ceb57db2ea4ef850cd3bd2
parth bhutada <parthbhutada13@gmail.com>
2025-02-03 20:43:00 +0530

    Creating Difference function
    This function creates a new array that stores the value of differences between two consecutive elements from the input array and also returns the Minimum difference value.

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -21,3 +21,25 @@ public void oddEven(int[] array) {
         System.out.println("Odd numbers:");
         display(odd); 
     }
+
+public void difference(int[] array) {
+        ArrayList<Integer> diff = new ArrayList<>();
+
+        for (int j = 0; j < array.length - 1; j++) {
+            diff.add(array[j] - array[j + 1]);
+        }
+        System.out.println("Differences:");
+        display(diff); // Display method refers to the ArrayList diff
+
+        int minIndex = 0;
+        int minValue = diff.get(0);
+
+        for (int i = 1; i < diff.size(); i++) {
+            if (diff.get(i) < minValue) {
+                minValue = diff.get(i);
+                minIndex = i;
+            }
+        }
+        System.out.println("Smallest Index: " + (minIndex + 1));
+    }
+}
